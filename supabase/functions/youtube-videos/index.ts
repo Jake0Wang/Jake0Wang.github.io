9462cb6e39d18ad3fa6e7fa61f72effc22f1d992
Jake0Wang <wangzhi444888488@gmail.com>
2023-04-15 03:18:50 -0400

    add supabase

---
+++ supabase/functions/youtube-videos/index.ts
@@ -0,0 +1,35 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "http";
+import { createClient } from "supabase";
+
+console.log("Hello from Functions!");
+
+serve(async () => {
+  const supabaseClient = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+    {}
+  );
+
+  const { error: matchError } = await supabaseClient
+    .from("youtube_videos")
+    .insert({
+      video_code: "test",
+      title: "test",
+    });
+
+  if (matchError) throw matchError;
+
+  return new Response(null, {
+    headers: { "Content-Type": "application/json" },
+  });
+});
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
